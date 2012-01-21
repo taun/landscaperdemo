4d9e0c6a21179fd01c15d643b51f13af14c04ec8
Taun Chapman <taun@ganymede-2.local>
2012-01-21 13:00:28 -0500

    Added core data storage for fractal rules, ...

--- LandscaperDemo/MBAppDelegate.h
+++ LandscaperDemo/MBAppDelegate.h
@@ -3,7 +3,7 @@
 //  LandscaperDemo
 //
 //  Created by Taun Chapman on 12/23/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 MOEDAE LLC. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
@@ -11,5 +11,17 @@
 @interface MBAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+-(BOOL)coreDataDefaultsExist;
+-(void)addDefaultCoreDataData;
+-(void)addDefaultDrawingRules;
+-(void)addDefaultLSFractals;
 
 @end
--- LandscaperDemo/MBAppDelegate.m
+++ LandscaperDemo/MBAppDelegate.m
@@ -3,18 +3,28 @@
 //  LandscaperDemo
 //
 //  Created by Taun Chapman on 12/23/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 MOEDAE LLC. All rights reserved.
 //
 
 #import "MBAppDelegate.h"
+#import "LSFractal.h"
+#import "LSReplacementRule.h"
+#import "LSDrawingRuleType.h"
+#import "LSDrawingRule.h"
 
 @implementation MBAppDelegate
 
 @synthesize window = _window;
+@synthesize managedObjectContext = __managedObjectContext;
+@synthesize managedObjectModel = __managedObjectModel;
+@synthesize persistentStoreCoordinator = __persistentStoreCoordinator;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    if (![self coreDataDefaultsExist]) {
+        [self addDefaultCoreDataData];
+    }
     return YES;
 }
 							
@@ -55,6 +65,188 @@
      Save data if appropriate.
      See also applicationDidEnterBackground:.
      */
+}
+
+- (void)saveContext
+{
+    NSError *error = nil;
+    NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+    if (managedObjectContext != nil)
+    {
+        if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error])
+        {
+            /*
+             Replace this implementation with code to handle the error appropriately.
+             
+             abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
+             */
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        } 
+    }
+}
+
+-(BOOL)coreDataDefaultsExist {
+    BOOL result = NO;
+    
+    NSManagedObjectContext* context = self.managedObjectContext;
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"LSFractal" inManagedObjectContext:context];
+    [fetchRequest setEntity:entity];
+    
+    NSError *error = nil;
+    NSArray *fetchedObjects = [context executeFetchRequest:fetchRequest error:&error];
+    if (fetchedObjects == nil) {
+        // TODO: error handling
+    } else if ([fetchedObjects count]>0){
+        // really basic test, could be much more complete
+        result = YES;
+    }
+    
+    return result;
+}
+
+-(void) addDefaultCoreDataData {
+    [self addDefaultDrawingRules];
+    [self addDefaultLSFractals];
+    [self saveContext];
+}
+
+-(void)addDefaultDrawingRules {
+    NSString *errorDesc = nil;
+    NSPropertyListFormat format;
+
+    NSString* plistPath = [[NSBundle mainBundle] pathForResource:@"DefaultLSDrawingRules" ofType:@"plist"];
+    NSData *plistXML = [[NSFileManager defaultManager] contentsAtPath:plistPath];
+    NSDictionary *tempDefaults = (NSDictionary *)[NSPropertyListSerialization
+                                          propertyListFromData:plistXML
+                                          mutabilityOption:NSPropertyListMutableContainersAndLeaves
+                                          format:&format
+                                          errorDescription:&errorDesc];
+
+    if (!tempDefaults) {
+        NSLog(@"Error reading plist: %@, format: %d", errorDesc, format);
+    } else {
+        NSManagedObjectContext* context = self.managedObjectContext;
+        
+        LSDrawingRuleType* defaultType = [NSEntityDescription
+                                               insertNewObjectForEntityForName:@"LSDrawingRuleType"
+                                               inManagedObjectContext: context];
+        defaultType.name = @"Default";
+        defaultType.identifier = @"default";
+        defaultType.descriptor = @"Default drawing rules added when the application is run for the first time.";
+        
+        NSDictionary* rules = [tempDefaults objectForKey: @"DrawingRules"];
+        for (NSString* key in rules) {
+            
+            LSDrawingRule *newDrawingRule = [NSEntityDescription
+                                                  insertNewObjectForEntityForName:@"LSDrawingRule"
+                                                  inManagedObjectContext: context];
+            newDrawingRule.type = defaultType;
+            newDrawingRule.productionString = key;
+            newDrawingRule.drawingMethodString = [rules objectForKey: key];
+
+        }
+    }
+}
+
+-(void)addDefaultLSFractals {
+    
+}
+
+#pragma mark - Core Data stack
+
+/**
+ Returns the managed object context for the application.
+ If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
+ */
+- (NSManagedObjectContext *)managedObjectContext
+{
+    if (__managedObjectContext != nil)
+    {
+        return __managedObjectContext;
+    }
+    
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (coordinator != nil)
+    {
+        __managedObjectContext = [[NSManagedObjectContext alloc] init];
+        [__managedObjectContext setPersistentStoreCoordinator:coordinator];
+    }
+    return __managedObjectContext;
+}
+
+/**
+ Returns the managed object model for the application.
+ If the model doesn't already exist, it is created from the application's model.
+ */
+- (NSManagedObjectModel *)managedObjectModel
+{
+    if (__managedObjectModel != nil)
+    {
+        return __managedObjectModel;
+    }
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"FSLibrary" withExtension:@"momd"];
+    __managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    return __managedObjectModel;
+}
+
+/**
+ Returns the persistent store coordinator for the application.
+ If the coordinator doesn't already exist, it is created and the application's store added to it.
+ */
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
+{
+    if (__persistentStoreCoordinator != nil)
+    {
+        return __persistentStoreCoordinator;
+    }
+    
+    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"FSLibrary.sqlite"];
+    
+    NSError *error = nil;
+    __persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+    if (![__persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error])
+    {
+        /*
+         Replace this implementation with code to handle the error appropriately.
+         
+         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
+         
+         Typical reasons for an error here include:
+         * The persistent store is not accessible;
+         * The schema for the persistent store is incompatible with current managed object model.
+         Check the error message to determine what the actual problem was.
+         
+         
+         If the persistent store is not accessible, there is typically something wrong with the file path. Often, a file URL is pointing into the application's resources directory instead of a writeable directory.
+         
+         If you encounter schema incompatibility errors during development, you can reduce their frequency by:
+         * Simply deleting the existing store:
+         [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil]
+         
+         * Performing automatic lightweight migration by passing the following dictionary as the options parameter: 
+         [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES], NSMigratePersistentStoresAutomaticallyOption, [NSNumber numberWithBool:YES], NSInferMappingModelAutomaticallyOption, nil];
+         
+         Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
+         
+         */
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        abort();
+    }    
+    
+    return __persistentStoreCoordinator;
+}
+
+#pragma mark - Application's Documents directory
+
+/**
+ Returns the URL to the application's Documents directory.
+ */
+- (NSURL *)applicationDocumentsDirectory
+{
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
 }
 
 @end
