1c891cde3edc081da4ed2648e82c629ad0842d0d
Taun Chapman <taun@ganymede-2.local>
2012-02-22 19:23:42 -0500

    pre table branch

--- LandscaperDemoTests/LandscaperDemoTests.h
+++ LandscaperDemoTests/LandscaperDemoTests.h
@@ -3,7 +3,7 @@
 //  LandscaperDemoTests
 //
 //  Created by Taun Chapman on 12/23/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 MOEDAE LLC. All rights reserved.
 //
 
 #import <SenTestingKit/SenTestingKit.h>
