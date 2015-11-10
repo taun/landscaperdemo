ff81b43904a3405fa92304feccf3fb3f4922a67f
Taun Chapman <tchapman@moedae.com>
2015-11-09 23:07:17 -0500

    Fixed Cloud Browser Cell Fonts

--- FractalScape/MDBPurchaseManager.m
+++ FractalScape/MDBPurchaseManager.m
@@ -520,7 +520,7 @@
         }
         
 #pragma message "UpdateForBuilds to build version before archiving"
-        const char *referenceVersion =  "430";
+        const char *referenceVersion =  "431";
         const char *receiptVersion = (const char *)bundle_version->buf+2;
         if (strcmp(referenceVersion,receiptVersion))
         {
