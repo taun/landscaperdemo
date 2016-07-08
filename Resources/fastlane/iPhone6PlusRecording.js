e06ef30afa8aff7131c4a14091d1bc8b98307488
Taun Chapman <tchapman@moedae.com>
2016-07-08 18:12:05 -0400

    Rearranging to add project resources which were outside of git folder into the git folder

---
+++ Resources/fastlane/iPhone6PlusRecording.js
@@ -0,0 +1,58 @@
+
+var target = UIATarget.localTarget();
+
+target.frontMostApp().mainWindow().scrollViews()[0].staticTexts()["FractalScapes let's you create new colorful and amazing fractal plants and patterns. You don't need to be an artist to create cool looking designs.\n\nJust play around with the fractals and have fun. We have added sample fractals to play with. If you want more, you can browse our in-app public Cloud share. Or upgrade to the Pro version and create your own from scratch.\n\nThe main screen with all your fractals is called your \"Library\". See below for a description of each of the library Tabs."].scrollToVisible();
+target.frontMostApp().mainWindow().scrollViews()[0].buttons()["Get Started by selecting a fractal!"].tap();
+target.frontMostApp().mainWindow().collectionViews()[0].cells()["Sierpinski Gasket No Fills"].staticTexts()["The classic Sierpinski. You can have fun adding the curve rule or playing with fills and filters. "].scrollToVisible();
+target.frontMostApp().mainWindow().collectionViews()[0].cells()["Snowflake"].scrollToVisible();
+target.frontMostApp().mainWindow().collectionViews()[0].cells()["Von Koch Curvy "].staticTexts()["Modified"].scrollToVisible();
+target.frontMostApp().mainWindow().collectionViews()[0].cells()["Dragon  Curved Stylized"].tap();
+target.frontMostApp().navigationBar().leftButton().tap();
+target.frontMostApp().mainWindow().buttons()["OpenCloseEditor"].tap();
+target.frontMostApp().mainWindow().popover().tabBar().buttons()["Colors"].tap();
+target.frontMostApp().mainWindow().popover().tabBar().buttons()["Effects"].tap();
+target.frontMostApp().mainWindow().popover().tabBar().buttons()["Rules"].tap();
+target.frontMostApp().mainWindow().buttons()["OpenCloseEditor"].tap();
+target.frontMostApp().navigationBar().buttons()["toolBarPlay"].tap();
+target.frontMostApp().mainWindow().scrollViews()[0].images()[0].tapWithOptions({tapOffset:{x:0.92, y:0.54}});
+target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.69);
+target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.88);
+target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.99);
+target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.77);
+target.frontMostApp().navigationBar().buttons()["Stop"].tap();
+target.frontMostApp().navigationBar().rightButton().tap();
+target.frontMostApp().actionSheet().buttons()["Cancel"].tap();
+target.frontMostApp().navigationBar().leftButton().tap();
+
+
+var target = UIATarget.localTarget();
+
+target.frontMostApp().mainWindow().scrollViews()[0].staticTexts()["FractalScapes let's you create new colorful and amazing fractal plants and patterns. You don't need to be an artist to create cool looking designs.\n\nJust play around with the fractals and have fun. We have added sample fractals to play with. If you want more, you can browse our in-app public Cloud share. Or upgrade to the Pro version and create your own from scratch.\n\nThe main screen with all your fractals is called your \"Library\". See below for a description of each of the library Tabs."].scrollToVisible();
+target.frontMostApp().mainWindow().scrollViews()[0].buttons()["Get Started by selecting a fractal!"].tap();
+target.frontMostApp().mainWindow().collectionViews()[0].cells()["Sierpinski Gasket No Fills"].staticTexts()["The classic Sierpinski. You can have fun adding the curve rule or playing with fills and filters. "].scrollToVisible();
+target.frontMostApp().mainWindow().collectionViews()[0].cells()["Snowflake"].scrollToVisible();
+target.frontMostApp().mainWindow().collectionViews()[0].cells()["Von Koch Curvy "].staticTexts()["Modified"].scrollToVisible();
+target.frontMostApp().mainWindow().collectionViews()[0]target.frontMostApp().mainWindow().collectionViews()[0].cells()["Dragon Plain"].scrollToVisible();
+                       target.frontMostApp().mainWindow().collectionViews()[0].dragInsideWithOptions({startOffset:{x:0.54, y:0.59}, endOffset:{x:0.55, y:0.36}});
+                       target.frontMostApp().mainWindow().collectionViews()[0].cells()["Cool Red Tree WIth Berries"].tap();
+                       target.frontMostApp().mainWindow().scrollViews()[0].images()[0].tapWithOptions({tapOffset:{x:0.51, y:0.45}});
+                       target.frontMostApp().mainWindow().scrollViews()[0].images()[0].tapWithOptions({tapOffset:{x:0.51, y:0.45}});
+                       target.frontMostApp().navigationBar().rightButton().tap();
+                       target.frontMostApp().actionSheet().buttons()["Cancel"].tap();
+                       .tap();
+target.frontMostApp().navigationBar().leftButton().tap();
+target.frontMostApp().mainWindow().buttons()["OpenCloseEditor"].tap();
+target.frontMostApp().mainWindow().popover().tabBar().buttons()["Colors"].tap();
+target.frontMostApp().mainWindow().popover().tabBar().buttons()["Effects"].tap();
+target.frontMostApp().mainWindow().popover().tabBar().buttons()["Rules"].tap();
+target.frontMostApp().mainWindow().buttons()["OpenCloseEditor"].tap();
+target.frontMostApp().navigationBar().buttons()["toolBarPlay"].tap();
+target.frontMostApp().mainWindow().scrollViews()[0].images()[0].tapWithOptions({tapOffset:{x:0.92, y:0.54}});
+target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.69);
+target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.88);
+target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.99);
+target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.77);
+target.frontMostApp().navigationBar().buttons()["Stop"].tap();
+target.frontMostApp().navigationBar().rightButton().tap();
+target.frontMostApp().actionSheet().buttons()["Cancel"].tap();
+target.frontMostApp().navigationBar().leftButton().tap();
---
+++ Resources/fastlane/snapshot.js
@@ -0,0 +1,98 @@
+#import "SnapshotHelper.js"
+
+var target = UIATarget.localTarget();
+var app = target.frontMostApp();
+var window = app.mainWindow();
+
+
+target.delay(2)
+window.scrollViews()[0].buttons()["Get Started by selecting a fractal!"].tap();
+target.delay(2)
+captureLocalizedScreenshot("0-LandingScreen")
+
+window.collectionViews()[0].cells()[10].tap();
+//target.frontMostApp().mainWindow().buttons()["OpenCloseEditor"].tap();
+//target.frontMostApp().mainWindow().buttons()["ToggleHUDViews"].tap();
+//target.frontMostApp().navigationBar().buttons()["toolBarPlay"].tap();
+//target.frontMostApp().navigationBar().buttons()["Stop"].tap();
+//target.frontMostApp().navigationBar().buttons()["toolBarPlay"].tap();
+//target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.48);
+//target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.67);
+//target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.73);
+//target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.77);
+//target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.45);
+//target.frontMostApp().navigationBar().buttons()["Stop"].tap();
+//target.frontMostApp().navigationBar().leftButton().tap();
+//target.frontMostApp().navigationBar().rightButton().tap();
+//target.frontMostApp().navigationBar().rightButton().tap();
+//target.frontMostApp().statusBar().tapWithOptions({tapOffset:{x:0.72, y:0.90}});
+//target.frontMostApp().mainWindow().popover().dismiss();
+//target.frontMostApp().navigationBar().leftButton().tap();
+//target.frontMostApp().tabBar().buttons()["Browse Cloud"].tap();
+// Alert detected. Expressions for handling alerts should be moved into the UIATarget.onAlert function definition.
+//target.frontMostApp().alert().buttons()["OK"].tap();
+
+target.delay(2)
+app.navigationBar().leftButton().tap();
+
+target.delay(6)
+captureLocalizedScreenshot("1-DragonView")
+
+target.delay(1)
+window.buttons()["OpenCloseEditor"].tap();
+target.delay(2)
+captureLocalizedScreenshot("2-DragonEditDescription")
+
+window.popover().tabBar().buttons()["Colors"].tap();
+target.delay(2)
+captureLocalizedScreenshot("3-DragonEditColors")
+
+window.popover().tabBar().buttons()["Effects"].tap();
+target.delay(2)
+captureLocalizedScreenshot("4-DragonEditEffects")
+
+window.popover().tabBar().buttons()["Rules"].tap();
+target.delay(2)
+captureLocalizedScreenshot("5-DragonEditRules")
+
+//target.frontMostApp().mainWindow().buttons()["OpenCloseEditor"].tap();
+window.elements()["OpenCloseEditor"].tapWithOptions({tapOffset:{x:0.82, y:0.51}});
+target.delay(1)
+//target.frontMostApp().mainWindow().buttons()["ToggleHUDViews"].tap();
+//window.buttons()["toolBarHUDScreenIcon"].tap();
+target.delay(1)
+
+captureLocalizedScreenshot("6-DragonFullScreen")
+
+app.navigationBar().buttons()["toolBarPlay"].tap();
+target.delay(2)
+target.frontMostApp().mainWindow().sliders()["PlayBackSlider"].dragToValue(0.77);
+//window.sliders()[0].dragToValue(0.69);
+
+captureLocalizedScreenshot("7-DragonPlay")
+
+app.navigationBar().buttons()["Stop"].tap();
+target.delay(1)
+
+app.navigationBar().rightButton().tap();
+
+target.delay(1)
+captureLocalizedScreenshot("8-DragonShareOptions")
+
+app.actionSheet().buttons()["Cancel"].tap();
+target.delay(1)
+app.navigationBar().leftButton().tap();
+target.delay(1)
+app.navigationBar().rightButton().tap();
+
+target.delay(1)
+window.collectionViews()[0].cells()[10].tap();
+
+target.delay(1)
+app.navigationBar().rightButton().tap();
+
+target.delay(2)
+captureLocalizedScreenshot("9-LibraryCloudShare")
+
+app.actionSheet().buttons()["Cancel"].tap();
+app.navigationBar().leftButton().tap();
