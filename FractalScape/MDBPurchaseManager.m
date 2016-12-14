de60eb33c113d68914c796b1c76c8d094e814d41
Taun Chapman <tchapman@moedae.com>
2016-12-14 01:09:53 -0500

    Version bump Motion NSMotionUsageDescription

--- FractalScape/MDBPurchaseManager.m
+++ FractalScape/MDBPurchaseManager.m
@@ -522,7 +522,7 @@
         }
         
 #pragma message "UpdateForBuilds to build version before archiving"
-        const char *referenceVersion =  "478";
+        const char *referenceVersion =  "479";
         const char *receiptVersion = (const char *)bundle_version->buf+2;
         if (strcmp(referenceVersion,receiptVersion))
         {
