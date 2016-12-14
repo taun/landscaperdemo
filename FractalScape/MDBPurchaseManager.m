3c8d5eff8aabcb7a2047d13116fff1eff747c27c
Taun Chapman <tchapman@moedae.com>
2016-12-14 00:39:45 -0500

    Version 1.1.0 RC1

--- FractalScape/MDBPurchaseManager.m
+++ FractalScape/MDBPurchaseManager.m
@@ -522,7 +522,7 @@
         }
         
 #pragma message "UpdateForBuilds to build version before archiving"
-        const char *referenceVersion =  "477";
+        const char *referenceVersion =  "478";
         const char *receiptVersion = (const char *)bundle_version->buf+2;
         if (strcmp(referenceVersion,receiptVersion))
         {
